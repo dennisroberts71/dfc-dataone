a637a3c7ba7c2f324d64f88aa1377e4a9b0efa53
michael-conway <michael_conway@unc.edu>
2017-01-26 18:12:05 -0500

    #2 hold open space for abstract api for pid and event

--- dataone-node/src/main/java/org/irods/jargon/dataone/utils/RestVersion.java
+++ dataone-node/src/main/java/org/irods/jargon/dataone/utils/RestVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.dataone.utils;
 public final class RestVersion {
  public static String VERSION="4.1.10.0-SNAPSHOT";
- public static String BUILD_TIME="2017-01-26T22:42:52Z";
+ public static String BUILD_TIME="2017-01-26T23:09:49Z";
 }

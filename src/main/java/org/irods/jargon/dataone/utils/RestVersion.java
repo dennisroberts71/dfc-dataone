3b8f132757b32d83b401be253e2a2ed6458ff3d9
michael-conway <michael_conway@unc.edu>
2017-01-26 17:40:38 -0500

    #1 cleanup

---
+++ src/main/java/org/irods/jargon/dataone/utils/RestVersion.java
@@ -0,0 +1,5 @@
+package org.irods.jargon.dataone.utils;
+public final class RestVersion {
+ public static String VERSION="4.1.10.0-SNAPSHOT";
+ public static String BUILD_TIME="2017-01-26T22:35:58Z";
+}

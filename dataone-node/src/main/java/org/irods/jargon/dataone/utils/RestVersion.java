4d9110cf3a7e16495db8cb47ed7bd4ab55c39040
michael-conway <michael_conway@unc.edu>
2017-03-20 12:32:46 -0400

    #1 cleanup after merge

--- dataone-node/src/main/java/org/irods/jargon/dataone/utils/RestVersion.java
+++ dataone-node/src/main/java/org/irods/jargon/dataone/utils/RestVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.dataone.utils;
 public final class RestVersion {
  public static String VERSION="4.2.1.0-SNAPSHOT";
- public static String BUILD_TIME="2017-03-20T16:17:38Z";
+ public static String BUILD_TIME="2017-03-20T16:28:46Z";
 }

c72ad0255c1a6d320dd207ba6efe79a70a0718ec
michael-conway <michael.c.conway@gmail.com>
2017-05-16 10:39:03 -0400

    #2 merge

--- dataone-node/src/main/java/org/irods/jargon/dataone/utils/RestVersion.java
+++ dataone-node/src/main/java/org/irods/jargon/dataone/utils/RestVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.dataone.utils;
 public final class RestVersion {
  public static String VERSION="4.2.1.0-SNAPSHOT";
- public static String BUILD_TIME="2017-03-10T15:08:18Z";
+ public static String BUILD_TIME="2017-05-16T14:37:56Z";
 }

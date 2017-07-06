0502e3e4930ea9b65f9f00d26eb1e2a720fae2ff
Dennis Roberts <dennis@cyverse.org>
2017-07-06 12:15:17 -0700

    #14: minor refactor

--- dataone-plugin/src/main/java/org/irods/jargon/dataone/config/CommonConfig.java
+++ dataone-plugin/src/main/java/org/irods/jargon/dataone/config/CommonConfig.java
@@ -1,28 +1,25 @@
 package org.irods.jargon.dataone.config;
 
 import org.dataone.service.types.v1.NodeReference;
+import org.irods.jargon.dataone.plugin.ConfigConstants;
 import org.irods.jargon.dataone.plugin.PublicationContext;
 
 /**
  * @author Dennis Roberts - CyVerse
  */
 public class CommonConfig {
-	private static final String CHECKSUM_ALGORITHM_PROPERTY = "irods.dataone.chksum-algorithm";
-	private static final String DATAONE_NODE_ID_PROPERTY = "irods.dataone.identifier";
-
-	private static final String DEFAULT_CHECKSUM_ALGORITHM = "MD5";
-	private static final String DEFAULT_DATAONE_NODE_ID = "fake-node-id";
-
 	// Prevent instantiation.
 	private CommonConfig() {
 	}
 
 	public static String getChecksumAlgorithm(PublicationContext ctx) {
-		return ctx.getAdditionalProperties().getProperty(CHECKSUM_ALGORITHM_PROPERTY, DEFAULT_CHECKSUM_ALGORITHM);
+		return ctx.getAdditionalProperties()
+				.getProperty(ConfigConstants.PROPERTY_CHECKSUM_ALGORITHM, ConfigConstants.DEFAULT_CHECKSUM_ALGORITHM);
 	}
 
 	public static String getDataOneNodeId(PublicationContext ctx) {
-		return ctx.getAdditionalProperties().getProperty(DATAONE_NODE_ID_PROPERTY, DEFAULT_DATAONE_NODE_ID);
+		return ctx.getAdditionalProperties()
+				.getProperty(ConfigConstants.PROPERTY_NODE_IDENTIFIER, ConfigConstants.DEFAULT_NODE_IDENTIFIER);
 	}
 
 	public static NodeReference getNodeReference(PublicationContext ctx) {

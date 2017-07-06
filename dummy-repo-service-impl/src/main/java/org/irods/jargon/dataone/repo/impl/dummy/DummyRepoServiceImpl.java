b424bf22a74a129459b23cf1e9776950cb9d6bea
Dennis Roberts <dennis@cyverse.org>
2017-07-06 12:56:49 -0700

    #14: removed DataObjectListResponse, which is no longer used

--- dummy-repo-service-impl/src/main/java/org/irods/jargon/dataone/repo/impl/dummy/DummyRepoServiceImpl.java
+++ dummy-repo-service-impl/src/main/java/org/irods/jargon/dataone/repo/impl/dummy/DummyRepoServiceImpl.java
@@ -3,21 +3,19 @@
  */
 package org.irods.jargon.dataone.repo.impl.dummy;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 import org.dataone.service.types.v1.Identifier;
 import org.dataone.service.types.v1.ObjectFormatIdentifier;
 import org.irods.jargon.core.connection.IRODSAccount;
-import org.irods.jargon.core.exception.FileNotFoundException;
 import org.irods.jargon.core.exception.JargonException;
 import org.irods.jargon.core.pub.domain.Collection;
 import org.irods.jargon.core.pub.domain.DataObject;
 import org.irods.jargon.dataone.model.DataOneObjectListResponse;
 import org.irods.jargon.dataone.plugin.PublicationContext;
 import org.irods.jargon.dataone.reposervice.AbstractDataOneRepoServiceAO;
-import org.irods.jargon.dataone.reposervice.DataObjectListResponse;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author mcc

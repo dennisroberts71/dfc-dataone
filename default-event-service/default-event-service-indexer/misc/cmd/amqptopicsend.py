50bae553b88b48c3128114299b8cf0eb26857985
michael-conway <michael.c.conway@gmail.com>
2017-07-14 08:22:00 -0400

    #20 indexer tested from CL

--- default-event-service/default-event-service-indexer/misc/cmd/amqptopicsend.py
+++ default-event-service/default-event-service-indexer/misc/cmd/amqptopicsend.py
@@ -9,32 +9,37 @@ import os
 import sys
 
 try:
-    logging.basicConfig(stream=sys.stdout)
+  
+	
+	logging.basicConfig(stream=sys.stdout)
+	
+	ephemeral = sys.argv[1].lower() == 'true'
+	 
+	key = sys.argv[2]
+	sys.stdout.write("key:" + key)
+	body = sys.argv[3]
+	sys.stdout.write("body:" + body)
+	
+	exchange = 'irods'
+	
+	connParams = pika.URLParameters("amqp://irods:irods@10.0.2.2") //FIXME: add as env param
+	
+	connection = pika.BlockingConnection(connParams)
+	sys.stdout.write("hello")
+	channel = connection.channel()
+	
+	channel.exchange_declare(exchange=exchange,
+	                             exchange_type='topic',
+	                             durable=(not ephemeral),
+	                             auto_delete=ephemeral)
+	
+	channel.basic_publish(exchange=exchange,
+	       routing_key=key,
+	       body=body,
+	       properties=pika.BasicProperties(delivery_mode=2))
+	
+	connection.close()
 
-    ephemeral = sys.argv[1].lower() == 'true'
-    key = sys.argv[2]
-    body = sys.argv[3]
-
-    exchange = 'irods'
-
-    connParams = pika.URLParameters(os.environ['IRODS_AMQP_URI'])
-
-    connection = pika.BlockingConnection(connParams)
-    channel = connection.channel()
-
-    channel.exchange_declare(exchange=exchange,
-                             exchange_type='topic',
-                             durable=(not ephemeral),
-                             auto_delete=ephemeral
-    )
-
-    channel.basic_publish(exchange=exchange,
-                          routing_key=key,
-                          body=body,
-                          properties=pika.BasicProperties(delivery_mode=2)
-    )
-
-    connection.close()
 except BaseException as e:
     sys.stderr.write('ERROR: %s\n' % e)
     sys.exit(1)

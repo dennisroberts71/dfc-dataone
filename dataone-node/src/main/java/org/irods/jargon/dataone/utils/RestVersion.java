34b304024258f4847b45c744cd43d8dea5695087
michael-conway <michael.c.conway@gmail.com`>
2017-10-04 09:56:11 -0400

    #1 cleanups to correct build on new machine, pls test

--- dataone-node/src/main/java/org/irods/jargon/dataone/utils/RestVersion.java
+++ dataone-node/src/main/java/org/irods/jargon/dataone/utils/RestVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.dataone.utils;
 public final class RestVersion {
  public static String VERSION="4.2.1.0-SNAPSHOT";
- public static String BUILD_TIME="2017-10-04T13:05:43Z";
+ public static String BUILD_TIME="2017-10-04T13:54:11Z";
 }
